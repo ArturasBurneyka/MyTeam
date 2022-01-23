379ace5237d0590550e23151a61aa2aa65beba76
ArturasBurneyka <evacapperson@list.ru>
2022-01-23 15:43:00 -0500

    Delete and Update action methods have been added to PlayersController

--- Controllers/PlayersController.cs
+++ Controllers/PlayersController.cs
@@ -1,7 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using MyTeam.Models;
 
 namespace MyTeam.Controllers {
@@ -18,7 +20,7 @@ namespace MyTeam.Controllers {
         }
 
         /// <summary>
-        /// Getting all players
+        /// Getting all players (Получение всех игроков)
         /// </summary>
         [HttpGet]
         public IActionResult GetAll() {
@@ -48,7 +50,7 @@ namespace MyTeam.Controllers {
         }
 
         /// <summary>
-        /// Creating new player
+        /// Creating new player (Создание нового игрока)
         /// </summary>
         [HttpPost]
         public IActionResult Create([FromBody] CreatePlayerDto createPlayerDto) {
@@ -74,5 +76,38 @@ namespace MyTeam.Controllers {
             return Created("", null);
         }
 
+        /// <summary>
+        /// Deleting player (Удаление игрока)
+        /// </summary>
+        [HttpDelete]
+        public async Task<IActionResult> Delete([FromBody] DeletePlayerDto body) {
+            var player = await this._context.Players.FindAsync(body.Email);
+            if (player == null) {
+                return NotFound("Игрок с такой электронной почтой не зарегистрирован");
+            }
+
+            this._context.Players.Remove(player);
+            await this._context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Updating player (Обновление игрока)
+        /// </summary>
+        [HttpPut]
+        public async Task<IActionResult> Update([FromBody] UpdatePlayerDto body) {
+            var player = await this._context.Players.FindAsync(body.Email);
+            if (player == null) {
+                return NotFound("Игрок с такой электронной почтой не зарегистрирован");
+            }
+
+            player.Nick = body.Nick;
+            player.Type = body.Type;
+
+            await this._context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }        
